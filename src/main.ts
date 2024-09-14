8414c1e703b5507a9c6743c319ad5e695abe298c
nathanielyoon <nathanielyoon1@gmail.com>
2024-09-14 15:47:00 -0400

    save

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { $ } from "./lib/dom.ts";
 
 const a = $("code");
-const b = $("button");
+const b = $("div");
+const c = $("button");
+const d: { [_: string]: number }[] = [];
 
-b.addEventListener(
+c.addEventListener(
   "click",
-  () =>
-    navigator.geolocation.getCurrentPosition((position) =>
-      a.textContent = JSON.stringify(position, null, 2)
-    ),
+  () => {
+    b.textContent = "wait";
+    navigator.geolocation.getCurrentPosition(({ coords, timestamp }) => {
+      d.push({
+        timestamp,
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+      });
+      a.textContent = JSON.stringify(d, null, 2);
+      b.textContent = "ok";
+    });
+  },
 );
