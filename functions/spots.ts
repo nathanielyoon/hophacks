8aba866f74c523bac21bf501e3d11b9b52b367a7
nathanielyoon <nathanielyoon1@gmail.com>
2024-09-15 08:08:22 -0400

    test

--- functions/spots.ts
+++ functions/spots.ts
@@ -16,11 +16,5 @@ export const onRequestPost = (context: Context) =>
     .catch(error);
 export const onRequestPut = (context: Context) =>
   context.request.text()
-    .then((text) =>
-      new Response(JSON.stringify({
-        key: text.slice(0, 45),
-        json: text.slice(45),
-      }))
-    );
-// .then((text) => PUT(context.env, text.slice(0, 45), spot(text.slice(45))))
-// .catch(error);
+    .then((text) => PUT(context.env, text.slice(0, 45), spot(text.slice(45))))
+    .catch(error);
