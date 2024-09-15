1902ba2a1bf9a0e99d1c1d88c6451961b6069ef0
nathanielyoon <nathanielyoon1@gmail.com>
2024-09-15 07:39:34 -0400

    okma sectet key

--- src/index_page.ts
+++ src/index_page.ts
@@ -1,3 +1,4 @@
+import { generate } from "./lib/25519.ts";
 import { a_s58 } from "./lib/58.ts";
 import { add } from "./lib/dom.ts";
 
@@ -5,10 +6,14 @@ add("button", undefined, { textContent: "GET STARTED" }).addEventListener(
   "click",
   async function (this) {
     this.disabled = true;
-    const key = a_s58(crypto.getRandomValues(new Uint8Array(32)));
-    const response = await fetch("/spots", { method: "PUT", body: key + "[]" });
+    const secret_key = crypto.getRandomValues(new Uint8Array(32));
+    const public_key = a_s58(generate(secret_key));
+    const response = await fetch("/spots", {
+      method: "PUT",
+      body: public_key + "[]",
+    });
     const text = await response.text();
-    const url = `hophacks.nyoon.io/${key}`;
+    const url = `hophacks.nyoon.io/${secret_key}`;
     if (response.ok) {
       this.replaceWith(
         add("a", null, { href: "https://" + url, textContent: url }),
