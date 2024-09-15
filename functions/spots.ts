aaf95e2d18b17438a9818b03f08907cb15dff537
nathanielyoon <nathanielyoon1@gmail.com>
2024-09-14 22:40:29 -0400

    look lilfaok

--- functions/spots.ts
+++ functions/spots.ts
@@ -10,20 +10,21 @@ export const onRequestPost = on({
   console.log(body);
   const key = body.key;
   const url = presign(env, key);
-  console.log(url);
-  const page = compile(key, [{
-    label: "label",
-    lat: 0,
-    lon: 0,
-    alt: 0,
-    range: 5,
-  }]);
-  console.log(page);
-  const response = await fetch(url, {
-    method: "PUT",
-    body: page,
-    headers: { "cache-control": "no-cache", "content-type": "text/html" },
-  });
-  console.log(response, await response.text());
-  return new Response(response.ok ? ":)" : ":(");
+  return new Response(JSON.stringify({ url, env, body }));
+  // console.log(url);
+  // const page = compile(key, [{
+  //   label: "label",
+  //   lat: 0,
+  //   lon: 0,
+  //   alt: 0,
+  //   range: 5,
+  // }]);
+  // console.log(page);
+  // const response = await fetch(url, {
+  //   method: "PUT",
+  //   body: page,
+  //   headers: { "cache-control": "no-cache", "content-type": "text/html" },
+  // });
+  // console.log(response, await response.text());
+  // return new Response(response.ok ? ":)" : ":(");
 });
